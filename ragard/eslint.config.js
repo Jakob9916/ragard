4f102bfff9f11a196faebcdc74a3e7e69035ca1b
Jakob <jakoring100@student.kau.se>
2025-07-05 16:37:17 +0200

    First structure

---
+++ ragard/eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config([
+  globalIgnores(['dist', 'node_modules', 'build', '.next', '.vercel', 'coverage']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: globals.browser,
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'max-len': ['warn', { code: 100 }],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+])
