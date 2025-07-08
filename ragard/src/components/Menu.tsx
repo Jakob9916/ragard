10b8c8a3b9c3525b8c61350d4a84c446d653eabc
Jakob <jakoring100@student.kau.se>
2025-07-08 16:50:25 +0200

    Updated menu and logo

--- ragard/src/components/Menu.tsx
+++ ragard/src/components/Menu.tsx
@@ -1,27 +1,57 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Menu.scss'; // Import the styles for the menu
+import logo from '../assets/logo.png'; // Ändra till rätt filnamn om det är svg eller png
 
 const Menu = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className='menu'>
-      <ul className='menu-list'>
+      <div className='menu-logo'>
+        <Link to='/'>
+          <img src={logo} alt='Rågård logotyp' />
+        </Link>
+      </div>
+      <button
+        className={`menu-burger${open ? ' open' : ''}`}
+        aria-label='Öppna meny'
+        onClick={() => setOpen((o) => !o)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+      <ul className={`menu-list${open ? ' open' : ''}`}>
         <li className='menu-item'>
-          <Link to='/'>Home</Link>
+          <Link to='/' onClick={() => setOpen(false)}>
+            Hem
+          </Link>
         </li>
         <li className='menu-item'>
-          <Link to='/about'>About</Link>
+          <Link to='/about' onClick={() => setOpen(false)}>
+            Om oss
+          </Link>
         </li>
         <li className='menu-item'>
-          <Link to='/jobs'>Jobs</Link>
+          <Link to='/jobs' onClick={() => setOpen(false)}>
+            Jobb
+          </Link>
         </li>
         <li className='menu-item'>
-          <Link to='/partners'>Partners</Link>
+          <Link to='/partners' onClick={() => setOpen(false)}>
+            Partners
+          </Link>
         </li>
         <li className='menu-item'>
-          <Link to='/troubleshooting'>Troubleshooting</Link>
+          <Link to='/troubleshooting' onClick={() => setOpen(false)}>
+            Felsökning
+          </Link>
         </li>
         <li className='menu-item'>
-          <Link to='/video'>Video</Link>
+          <Link to='/video' onClick={() => setOpen(false)}>
+            Video
+          </Link>
         </li>
       </ul>
     </nav>
